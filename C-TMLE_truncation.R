535d7e9f399607127779da3a6c05dc86869866a0
Aurelien Bibaut <aurelien.bibaut@gmail.com>
2016-02-18 00:22:06 +0000

    Add MSEs computation and plotting. Add support for Q_misspecified.

--- C-TMLE_truncation.R
+++ C-TMLE_truncation.R
@@ -34,7 +34,7 @@ generate_data <- function(R, alpha0, beta0, beta1, beta2, n){
 }
 
 # C-TMLE_truncation
-C_TMLE_truncation <- function(observed_data, d0, deltas){
+C_TMLE_truncation <- function(observed_data, d0, deltas, Q_misspecified = F){
   
   L0 <- observed_data$L0; A0 <- observed_data$A0; L1 <- observed_data$L1
   n <- length(L0)
@@ -46,10 +46,14 @@ C_TMLE_truncation <- function(observed_data, d0, deltas){
   gn0 <- as.vector(predict(initial_model_for_A0, type="response"))
   
   # 1.a Fit initial model Q^1_{d,n} of Q^1_{0,d}
-  coeffs_Q1d_bar_0n <- optim(par=c(0,0,0), fn=loss, outcome=L1, 
-                                                covariates=cbind(1,L0,A0), 
-                                                boolean_subset=(A0==d0(L0)))$par
-  offset_vals_Q1d_bar_0n <- as.vector(cbind(1, L0, d0(L0)) %*% coeffs_Q1d_bar_0n)
+  if(Q_misspecified == FALSE){
+    coeffs_Q1d_bar_0n <- optim(par=c(0,0,0), fn=loss, outcome=L1, 
+                                                  covariates=cbind(1,L0,A0), 
+                                                  boolean_subset=(A0==d0(L0)))$par
+    offset_vals_Q1d_bar_0n <- as.vector(cbind(1, L0, d0(L0)) %*% coeffs_Q1d_bar_0n)
+  }else{
+    offset_vals_Q1d_bar_0n <- logit(mean(L1[A0==d0(L0)]))
+  }
   Q1d_bar_0n <- expit(offset_vals_Q1d_bar_0n)
   
   # Targeting steps: iterate targeting until either no likelihood gain is obtained,
@@ -76,7 +80,7 @@ C_TMLE_truncation <- function(observed_data, d0, deltas){
       # Fit parametric submodel to training set
       epsilon <- sum((((L1 - current_Q1d_bar_n) * H_delta) * (A0 == d0(L0)))[training_set_indices]) /
                            sum((H_delta^2 * (A0 == d0(L0)))[training_set_indices])
-      candidate_Q1d_bar_n <- current_Q1d_bar_n + epsilon * H_delta
+      candidate_Q1d_bar_n <- current_Q1d_bar_n + epsilon * H_delta_setting_A_to_d
       # Compute loss of the candidate on training set
       candidate_loss <- mean(((L1 - candidate_Q1d_bar_n)^2 * (A0 == d0(L0)))[test_set_indices])
       
@@ -124,8 +128,8 @@ Psi_d0 <- compute_Psi_d_MC(R = 4, alpha0 = 2, beta0 = -3, beta1 = -1.5, beta2 = 
 # It is fully characterized by the parameters_tuple_id that the batch corresponds to.
 ns <- c((1:9)*100, c(1:10)*1000)
 parameters_grid <- expand.grid(R = 4, alpha0 = 2, beta0 = -3, beta1 = -1.5, beta2 = -2, n = ns)
-batch_size <- 100; nb_batchs <- 100
-jobs <- kronecker(1:(dim(parameters_grid)[1]), rep(1, nb_batchs))
+batch_size <- 40; nb_batchs <- 72
+jobs <- kronecker(1:nrow(parameters_grid), rep(1, nb_batchs))
 
 # Perform the jobs in parallel
 library(doParallel); library(foreach)
@@ -141,10 +145,29 @@ results <- foreach(i=1:length(jobs)) %dopar% { #job is a parameter_tuple_id
     observed_data <- generate_data(R = parameters_grid[job,]$R, alpha0 = parameters_grid[job,]$alpha0, 
                                    beta0 = parameters_grid[job,]$beta0, beta1 = parameters_grid[job,]$beta1, 
                                    beta2 = parameters_grid[job,]$beta2, n = parameters_grid[job,]$n)
-    result_C_TMLE <- C_TMLE_truncation(observed_data, alwaysTreated0, deltas)
+    result_C_TMLE <- list(Utgtd_Psi_n = NA, Psi_n = NA)
+    try(result_C_TMLE <- C_TMLE_truncation(observed_data, alwaysTreated0, deltas, Q_misspecified = T))
     results_batch[i, ] <- c(job, result_C_TMLE$Utgtd_Psi_n, result_C_TMLE$Psi_n)
   }
   results_batch
 }
 
 stopCluster(cl)
+
+# Combine the results together
+full_results_matrix <- results[[1]]
+for(i in 2:length(results)){
+  full_results_matrix <- rbind(full_results_matrix, results[[i]])
+}
+
+# Compute the MSEs for each parameter tuple id
+MSEs <- matrix(NA, nrow = nrow(parameters_grid), ncol = 4)
+for(i in 1:nrow(parameters_grid)){
+  MSE_utgtd <- mean((full_results_matrix[full_results_matrix[,"parameters_tuple_id"] == i, "Utgtd"] - Psi_d0)^2)
+  MSE_C_TMLE <- mean((full_results_matrix[full_results_matrix[,"parameters_tuple_id"] == i, "C-TMLE"] - Psi_d0)^2)
+  MSEs[i, ] <- c(i, parameters_grid[i,]$n, MSE_utgtd, MSE_C_TMLE)
+}
+colnames(MSEs) <- c("parameter_tuple_id", "n", "MSE_utgtd", "MSE_C-TMLE")
+
+plot(MSEs[, "n"],  MSEs[, "n"]* MSEs[,"MSE_utgtd"])
+lines(MSEs[, "n"],  MSEs[, "n"]* MSEs[,"MSE_C-TMLE"])
