392d54cd826c09b94a953ad0cea17a437f3bd721
Aurelien Bibaut <aurelien.bibaut@gmail.com>
2016-03-14 01:31:08 -0700

    Correct C-TMLE_truncation-multi_orders-mpi-loglik_targeting.R. Each of the TMLE is computed with the naive glm fit as initial estimator. No refinement of the initial fit with by successive targeting steps. TMLE that is picked is the one that has best CV loss for I(A==d(W))*(Y-\bar{Q})^2. Cross validated variance of the influence curve is added to the CV loss.

--- C-TMLE_truncation-multi_orders-mpi-loglik_targeting.R
+++ C-TMLE_truncation-multi_orders-mpi-loglik_targeting.R
@@ -10,12 +10,12 @@ logit <- function(x){
 
 expit<-function(x){
   result<-exp(x)/(1+exp(x))
-  result[is.nan(result)]<-1
+  result[is.nan(result)] <- 1
   result
 }
 
 g_to_g_delta<-function(delta, g){
-  (g<delta)*delta+(g>=delta)*g
+  (g<delta) * delta + (g>=delta) * g
 }
 
 loss <- function(coefficients, outcome, covariates, boolean_subset, offset=0){
@@ -45,23 +45,21 @@ compute_a_delta0 <- function(delta0, order, n_points = 9, diff_step = NULL, verb
   if(is.null(diff_step)){
     if(delta0 - (n_points-1)/2*1e-3 > 0){ diff_step=1e-3 }else{ diff_step = delta0/(n_points-1) }
   }
-  bw <- diff_step*2
-  deltas <- delta0 + (1:n_points-1-(n_points-1)/2)*diff_step
-  weights <- exp(-(deltas-delta0)^2/(2*bw^2))/sqrt(2*pi*bw^2)
+  bw <- diff_step * 2
+  deltas <- delta0 + (1:n_points-1-(n_points-1) / 2)*diff_step
+  weights <- exp(-(deltas-delta0)^2 / (2*bw^2)) / sqrt(2*pi*bw^2)
   
   X <- outer(deltas - delta0, 0:order, "^")
   
-  
   A <- apply(diag(nrow(X)), 2, function(C) lm.wfit(X, C, weights)$coefficients)
   a_delta0 <- (-delta0)^(0:order) %*% A
-  list(a_delta0 = a_delta0, deltas = deltas)
+  list(a_delta0 = a_delta0, differentiator = A, deltas = deltas)
 }
 
 # debug(compute_a_delta0)
 
-# C-TMLE_truncation
-C_TMLE_truncation <- function(observed_data, d0, orders, delta0s, Q_misspecified = F, 
-                              n_points = 11, diff_step = NULL){
+# TMLE of truncated target parameter
+TMLE_truncated_target <- function(observed_data, d0, delta, Q_misspecified = F){
   
   L0 <- observed_data$L0; A0 <- observed_data$A0; L1 <- observed_data$L1
   n <- length(L0)
@@ -74,83 +72,142 @@ C_TMLE_truncation <- function(observed_data, d0, orders, delta0s, Q_misspecified
   # 1.a Fit initial model Q^1_{d,n} of Q^1_{0,d}
   if(Q_misspecified == FALSE){
     coeffs_Q1d_bar_0n <- optim(par=c(0,0,0), fn=loss, outcome=L1, 
-                                                  covariates=cbind(1,L0,A0), 
-                                                  boolean_subset=(A0==d0(L0)))$par
+                               covariates=cbind(1,L0,A0), 
+                               boolean_subset=(A0==d0(L0)))$par
     offset_vals_Q1d_bar_0n <- as.vector(cbind(1, L0, d0(L0)) %*% coeffs_Q1d_bar_0n)
   }else{
     offset_vals_Q1d_bar_0n <- rep(logit(mean(L1[A0==d0(L0)])), n)
   }
   Q1d_bar_0n <- expit(offset_vals_Q1d_bar_0n)
   
-  # Cross validated targeting: compute the loss for \bar{Q} for 
-  # every (order, delta0) pair.
-  test_set_indices <- sample(1:n, floor(n/5), replace = F)
-  training_set_indices <- setdiff(1:n, test_set_indices)
-  best_loss <- mean(((L1 - Q1d_bar_0n)^2 * (A0 == d0(L0)))[test_set_indices])
-  best_Q1d_bar_n <- Q1d_bar_0n
-  best_indices <- list(order = -1, delta0 = 0) #(-1,0) stands for no targeting
+  # Compute clever covariate
+  gn0_delta <- g_to_g_delta(delta, gn0)
+  H_delta <- (A0 == d0(L0)) / gn0_delta
+  H_delta_setting_A_to_d <- 1 / gn0_delta
   
-  for(order in orders){
-    for(delta0 in delta0s){
-#       cat("Starting targeting step for (", order, ", ", delta0, ")\n")
-      
-      # Define test set and training set
-      test_set_indices <- sample(1:n, floor(n/5), replace = F)
-      training_set_indices <- setdiff(1:n, test_set_indices)
-      
-      # Compute a_delta0 as defined in write-up
-      result_compute_a_delta0 <- compute_a_delta0(delta0, order = order, n_points, diff_step, verbose=F)
-      a_delta0 <- result_compute_a_delta0$a_delta0
-      deltas <- result_compute_a_delta0$deltas
-      
-      # Compute clever covariate
-      gn0_delta0 <- g_to_g_delta(delta0, gn0)
-      gn0_deltas <- sapply(deltas, g_to_g_delta, g=gn0)
-      H_delta <- (A0==d0(L0)) / gn0 * (outer(gn0, rep(1, length(deltas))) / gn0_deltas) %*% t(a_delta0)
-      H_delta_setting_A_to_d <- 1 / gn0 * (outer(gn0, rep(1, length(deltas))) / gn0_deltas) %*% t(a_delta0)
-      
-      # Fit parametric submodel to training set
-      epsilon <- glm(L1 ~ H_delta - 1, family = binomial, offset = Q1d_bar_0n,
-                     subset = intersect(which(A0 == d0(L0)), training_set_indices))$coefficients[1]
-      candidate_Q1d_bar_n <- expit(logit(Q1d_bar_0n) + epsilon * H_delta_setting_A_to_d)
-      
-      # Compute loss of the candidate on training set. Update best loss and best candidate
-      # if needed
-      candidate_loss <- mean(((L1 - candidate_Q1d_bar_n)^2 * (A0 == d0(L0)))[test_set_indices])
-      
-      if(is.nan(candidate_loss)){
-        cat("order = ", order, "and delta0 = ", delta0, "\n")
-        cat("candidate_loss = ", candidate_loss, " and best_loss = ", best_loss, "\n")
-        #browser()
-      }
-      if(candidate_loss < best_loss){
-        best_loss <- candidate_loss
-        best_Q1d_bar_n <- candidate_Q1d_bar_n
-        best_indices <- list(order = order, delta0 = delta0)
-      }
-    }
-  }
-  # End of cross validated targeting
+  # Fit parametric submodel to training set
+  epsilon <- glm(L1 ~ H_delta - 1, family = binomial, offset = Q1d_bar_0n,
+                 subset = which(A0 == d0(L0)))$coefficients[1]
+  Q1d_bar_star_n <- expit(logit(Q1d_bar_0n) + epsilon * H_delta_setting_A_to_d)
   
-  # Compute estimate. 
-  # First recompute deltas, a_delta0, deltas and gn0_deltas for the pair of best indices
-  result_compute_a_delta0 <- compute_a_delta0(best_indices$delta0, order = best_indices$order, 
-                                              n_points, diff_step, verbose = F)
+  # Return estimator
+  mean(gn0 / gn0_delta * Q1d_bar_star_n)
+}
+
+# Untargeted extrapolation
+untargeted_extrapolation <- function(observed_data, d0, order, delta0, Q_misspecified = F,
+                                     n_points = 11, diff_step = NULL){
+  # Compute a_delta0 as defined in write-up
+  result_compute_a_delta0 <- compute_a_delta0(delta0, order = order, n_points, diff_step, verbose=F)
   a_delta0 <- result_compute_a_delta0$a_delta0
   deltas <- result_compute_a_delta0$deltas
-  gn0_deltas <- sapply(deltas, g_to_g_delta, g=gn0)
-  # Then compute the TMLE Psi_n(order, delta0)
-  Psi_n <- mean(best_Q1d_bar_n * (outer(gn0, rep(1, length(deltas))) / gn0_deltas) %*% t(a_delta0))
   
-  # For the record, compute the untargeted G-computation formula, and return outputs
-  Utgtd_Psi_n <- mean(Q1d_bar_0n)
-  list(Utgtd_Psi_n = Utgtd_Psi_n, Psi_n = Psi_n, tp_indices = best_indices)
+  # Get the Psi(delta) for each deltas
+  Psi_deltas <- sapply(deltas, function(delta) TMLE_truncated_target(observed_data, d0, delta, Q_misspecified = F))
+  
+  # Extrapolate
+  a_delta0 %*% Psi_deltas
+}
+
+# TMLE of extrapolation
+TMLE_extrapolation <- function(observed_data, training_set, test_set, d0, order, delta0, Q_misspecified = F, 
+                               n_points = 11, diff_step = NULL){
+  
+  L0 <- observed_data$L0; A0 <- observed_data$A0; L1 <- observed_data$L1
+  n <- length(L0)
+  
+  # Fit models for g_{n,k=0}
+  initial_model_for_A0 <- glm(A0 ~ 1 + L0, family=binomial, subset = training_set)
+  initial_model_for_A0$coefficients[is.na(initial_model_for_A0$coefficients)] <- 0
+  gn0 <- as.vector(predict(initial_model_for_A0, type="response"))
+  
+  # Compute a_delta0 as defined in write-up
+  result_compute_a_delta0 <- compute_a_delta0(delta0, order = order, n_points, diff_step, verbose=F)
+  a_delta0 <- result_compute_a_delta0$a_delta0
+  deltas <- result_compute_a_delta0$deltas
+  
+  # Compute clever covariate
+  gn0_delta0 <- g_to_g_delta(delta0, gn0)
+  gn0_deltas <- sapply(deltas, g_to_g_delta, g=gn0)
+  H_delta <- (A0==d0(L0)) / gn0 * (outer(gn0, rep(1, length(deltas))) / gn0_deltas) %*% t(a_delta0)
+  H_delta_setting_A_to_d <- 1 / gn0 * (outer(gn0, rep(1, length(deltas))) / gn0_deltas) %*% t(a_delta0)
+  
+  # Fit initial model Q^1_{d,n} of Q^1_{0,d}
+  if(Q_misspecified == FALSE){
+    coeffs_Q1d_bar_0n <- optim(par = c(0,0,0), fn=loss, outcome=L1, 
+                               covariates = cbind(1,L0,A0), 
+                               boolean_subset = intersect(which(A0 == d0(L0)), training_set))$par
+    offset_vals_Q1d_bar_0n <- as.vector(cbind(1, L0, d0(L0)) %*% coeffs_Q1d_bar_0n)
+  }else{
+    offset_vals_Q1d_bar_0n <- rep(logit(mean(L1[intersect(which(A0 == d0(L0)), training_set)])), n)
+  }
+  Q1d_bar_0n <- expit(offset_vals_Q1d_bar_0n)
+  
+  # Fit parametric submodel to training set
+  epsilon <- glm(L1 ~ H_delta - 1, family = binomial, offset = Q1d_bar_0n,
+                 subset = intersect(which(A0 == d0(L0)), training_set))$coefficients[1]
+  Q1d_bar_star_n <- expit(logit(Q1d_bar_0n) + epsilon * H_delta_setting_A_to_d)
+  
+  # Compute estimator and influence curve
+  Psi_n <- mean(Q1d_bar_star_n * (outer(gn0, rep(1, length(deltas))) / gn0_deltas) %*% t(a_delta0))
+  D_star_n <- (outer(gn0, rep(1, length(deltas))) / gn0_deltas) %*% t(a_delta0) #It's actually D_star_n plus its mean
+  var_D_star_n <- var(D_star_n)
+  var_D_star_n_test <- var(D_star_n[test_set])
+  
+  # Return estimator, and Q_bar
+  list(Psi_n = Psi_n, Q_bar = Q1d_bar_star_n, var_D_star_n = var_D_star_n, var_D_star_n_test = var_D_star_n_test)
 }
 
 
+# C-TMLE_truncation
+C_TMLE_truncation <- function(observed_data, d0, orders, delta0s, Q_misspecified = F, 
+                              n_points = 11, diff_step = NULL, K = 5){
+  
+  L0 <- observed_data$L0; A0 <- observed_data$A0; L1 <- observed_data$L1
+  n <- length(L0)
+  
+  # Define splits
+  split_sizes <- rep(floor(n / K), K)
+  if(n %% K != 0) for(i in 1:(n - floor(n / K) * K)) split_sizes[i] <- split_sizes[i] + 1
+  splits <- list(); splits_Comp <- list()
+  for(i in 1:K) splits[[i]] <- sum(split_sizes[0:(i-1)]):sum(split_sizes[0:i])
+  for(i in 1:K) splits_Comp[[i]] <- setdiff(1:n, splits[i])
+  
+  # Compute TMLEs and their cross validated losses
+  CV_losses <- matrix(0, nrow = length(orders), ncol = length(delta0s))
+  for(i in 1:length(orders)){
+    for(j in 1:length(delta0s)){
+      Q1d_bar <- list()
+      for(k in 1:K){ # Repeat the fitting procedure for the K splits
+        cat("i = ", i, ", j = ", j, ", k =", k, "\n")
+        result_TMLE_extrapolation <- TMLE_extrapolation(observed_data, splits_Comp[[k]], splits[[k]], d0, orders[i], delta0s[j])
+        Q1d_bar <- result_TMLE_extrapolation$Q_bar
+        var_IC_test <- result_TMLE_extrapolation$var_D_star_n_test
+        if(is.na(sum((A0[splits[[k]]] == d0(L0[splits[[k]]])) * 
+                       (L1[splits[[k]]] - Q1d_bar[splits[[k]]])^2))) browser()
+        CV_losses[i, j] <- CV_losses[i, j] + 
+          sum((A0[splits[[k]]] == d0(L0[splits[[k]]])) * 
+                        (L1[splits[[k]]] - Q1d_bar[splits[[k]]])^2) + 1 / K * var_IC_test
+      }
+    }
+  }
+  
+  # Compute estimate: pick the TMLE with lowest cross validated loss
+  # First recompute deltas, a_delta0, deltas and gn0_deltas for the pair of best indices
+  best_indices <- which(CV_losses == min(CV_losses), arr.ind = T)
+  best_order <- orders[best_indices[1]]; best_delta0 <- delta0s[best_indices[2]]
+  Psi_n <- TMLE_extrapolation(observed_data, 1:n, 1:n, d0, best_order, best_delta0)$Psi_n
+  
+  # Return output
+  list(Psi_n = Psi_n, CV_losses = CV_losses, tp_indices = list(order = best_order, delta0 = best_delta0))
+}
+
+# debug(C_TMLE_truncation)
+
 # Simulations -------------------------------------------------------------
 set.seed(0)
-delta0s <- c(1e-4, 5e-4, (1:9)*1e-3, (1:9)*1e-2, (1:4)*1e-1)
+delta0s <- c(1e-4, 1e-3, 1e-2, 1e-1)
+# delta0s <- c(1e-4, 5e-4, (1:9)*1e-3, (1:9)*1e-2, (1:4)*1e-1)
 # delta0s <- 1e-4
 orders <- 0:10
 # orders <- 9
@@ -165,16 +222,19 @@ compute_Psi_d_MC <- function(R, alpha0, beta0, beta1, beta2, d0, M){
   mean(PL1givenA0L0_MC)
 }
 
-Psi_d0 <- compute_Psi_d_MC(R = 4, alpha0 = 2, beta0 = -3, beta1 = -1.5, beta2 = -2, alwaysTreated0, M = 1e6)
+Psi_d0 <- compute_Psi_d_MC(R = 4, alpha0 = 2, beta0 = -3, beta1 = +1.5, beta2 = 1, alwaysTreated0, M = 1e6)
 
 # Specify the jobs. A job is the computation of a batch. 
 # It is fully characterized by the parameters_tuple_id that the batch corresponds to.
 ns <- c((1:9)*100, c(1:9)*1000, 2*c(1:5)*1e4)
-parameters_grid <- expand.grid(R = 4, alpha0 = 2, beta0 = -3, beta1 = -1.5, beta2 = -2, n = ns)
-batch_size <- 50; nb_batchs <- 200
+parameters_grid <- expand.grid(R = 4, alpha0 = 2, beta0 = -3, beta1 = +1.5, beta2 = 1, n = ns)
+batch_size <- 10; nb_batchs <- 100
 jobs <- kronecker(1:nrow(parameters_grid), rep(1, nb_batchs))
 jobs <- sample(jobs)
 
+
+# Save the parameters' grid
+write.table(parameters_grid, file = "parameters_grid.csv", append=F, row.names=F, col.names=T,  sep=",")
 
 # # Perform the jobs in parallel
 library(Rmpi); library(doMPI)
@@ -184,8 +244,8 @@ registerDoMPI(cl)
 
 results <- foreach(i=1:length(jobs)) %dopar% { #job is a parameter_tuple_id
   job <- jobs[i]
-  results_batch <- matrix(0, nrow = batch_size, ncol = 5)
-  colnames(results_batch) <- c("parameters_tuple_id", "Utgtd", "C-TMLE", "order", "delta0")
+  results_batch <- matrix(0, nrow = batch_size, ncol = 7)
+  colnames(results_batch) <- c("parameters_tuple_id", "EYd", "Utgtd-untr","Utgtd-extr", "C-TMLE", "order", "delta0")
   for(i in 1:batch_size){
     observed_data <- generate_data(R = parameters_grid[job,]$R, alpha0 = parameters_grid[job,]$alpha0, 
                                    beta0 = parameters_grid[job,]$beta0, beta1 = parameters_grid[job,]$beta1, 
@@ -193,8 +253,11 @@ results <- foreach(i=1:length(jobs)) %dopar% { #job is a parameter_tuple_id
     result_C_TMLE <- list(Utgtd_Psi_n = NA, Psi_n = NA)
     try(result_C_TMLE <- C_TMLE_truncation(observed_data, alwaysTreated0, orders, 
                                            delta0s, Q_misspecified = F))
-    print(result_C_TMLE)
-    results_batch[i, ] <- c(job, result_C_TMLE$Utgtd_Psi_n, result_C_TMLE$Psi_n, 
+    Utgtd_untr_Psi_n <- TMLE_truncated_target(observed_data, d0, 0, Q_misspecified = F)
+    Utgtd_extr_Psi_n <- untargeted_extrapolation(observed_data, d0,
+                                                 result_C_TMLE$tp_indices$order,
+                                                 result_C_TMLE$tp_indices$delta0, Q_misspecified = F)
+    results_batch[i, ] <- c(job, Psi_d0, result_C_TMLE$Psi_n, Utgtd_untr_Psi_n, Utgtd_extr_Psi_n,
                             result_C_TMLE$tp_indices$order, result_C_TMLE$tp_indices$delta0)
   }
 
