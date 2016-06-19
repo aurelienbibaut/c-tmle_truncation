4baddaf81e59d5cd7d0293b93e1e665a113aac23
Aurelien Bibaut <aurelien.bibaut@gmail.com>
2016-06-19 14:39:24 +0200

    Change target parameter for a still weakly identifiable one but easier to infer

--- data-adaptive_delta_selection/check_rates_bias_var_weakly_id_tp.R
+++ data-adaptive_delta_selection/check_rates_bias_var_weakly_id_tp.R
@@ -31,14 +31,23 @@ library(foreach); library(doParallel)
 # cat("beta = ", 1 - rate_b0, "\ngamma = ", abs(rate_sigma0))
 
 # Now let's check if (Psi_n(delta + Delta) - Psi(delta) + n^(-1/2) * ((delta+Delta)^-gamma - delta^-gamma)) / Delta
-beta <- 0.25
-gamma <- 0.125
-etas <- c(1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5)
+# Set of parameters 1
+# lambda <- 2; alpha0 <- 2; beta0 <- -3; beta1 <- 1.5; beta2 <- 1
+# beta <- 0.25
+# gamma <- 0.125
+# kappa <- 1 / (2 * (gamma + 1 - beta))
+# Set of parameters 2
+lambda <- 2; alpha0 <- 4; beta0 <- -3; beta1 <- 1.5; beta2 <- 0.5
+kappa <- 5 / 4
+beta <- 2 - kappa
+gamma <- 1 - kappa / 2
+
+etas <- c(3, 3.5, 4, 4.5, 5)
 finite_diffs <- vector(); finite_diffs_bias <- vector(); true_finite_diffs_bias <- vector()
 ns <- floor(10^seq(from = 2, to = 8, by = 0.5))
 deltas <- vector()
 
-observed_data <- generate_data("L0_exp", 2, 2, -3, 1.5, 1, max(ns))
+observed_data <- generate_data("L0_exp", lambda, alpha0, beta0, beta1, beta2, max(ns))
 # data <- data.frame(L0 = observed_data$L0, A0 = observed_data$A0, L1 = observed_data$L1)
 
 
@@ -46,7 +55,7 @@ observed_data <- generate_data("L0_exp", 2, 2, -3, 1.5, 1, max(ns))
 jobs <- expand.grid(n = ns, eta = etas)
 
 # Set up cluster
-cl <- makeCluster(getOption("cl.cores", 4), outfile = '')
+cl <- makeCluster(getOption("cl.cores", 5), outfile = '')
 registerDoParallel(cl)
 
 results <- foreach(i=1:nrow(jobs), .combine = rbind, 
@@ -56,40 +65,62 @@ results <- foreach(i=1:nrow(jobs), .combine = rbind,
                      
                      delta_n_plus <- n^(-1 / (2 * eta * (gamma + 1 - beta)))
                      
-                     deltas <- c(deltas, delta_n_plus)
-                     
                      Delta <- n^(-0.25) * delta_n_plus^((beta + 1 - gamma) / 2)
                      
                      cat('Job ', i, ', n = ', n, ', eta = ', eta, ', delta =', delta_n_plus, ', Delta = ', Delta, '\n')
                      Psi_n_delta_plus_Delta <- TMLE_EY1_speedglm(lapply(observed_data, function(x) x[1:n]), delta_n_plus + Delta)
-                     cat('Psi_n(delta + Delta) = ', Psi_n_delta_plus_Delta, '\n')
+                     cat('Job ', i, ', Psi_n(delta + Delta) = ', Psi_n_delta_plus_Delta, '\n')
                      Psi_n_delta <- TMLE_EY1_speedglm(lapply(observed_data, function(x) x[1:n]), delta_n_plus)
-                     cat('Psi_n(delta) = ', Psi_n_delta, '\n')
+                     cat('Job ', i, ',Psi_n(delta) = ', Psi_n_delta, '\n')
                      
-                     finite_diffs <- c(finite_diffs,
-                                       (Psi_n_delta_plus_Delta - Psi_n_delta + 
-                                          n^(-0.5) * ((delta_n_plus + Delta)^(-gamma) - delta_n_plus^(-gamma))) / Delta)
+                     finite_diffs <- (Psi_n_delta_plus_Delta - Psi_n_delta + 
+                                        n^(-0.5) * ((delta_n_plus + Delta)^(-gamma) - delta_n_plus^(-gamma))) / Delta
                      
-                     finite_diffs_bias <- c(finite_diffs_bias, (Psi_n_delta_plus_Delta - Psi_n_delta) / Delta)
+                     finite_diffs_bias <- (Psi_n_delta_plus_Delta - Psi_n_delta) / Delta
                      Psi_0_delta_plus_Delta <- compute_true_Psi0_delta("L0_exp", 2, 2, -3, 1.5, 1, alwaysTreated0, delta_n_plus + Delta)
                      Psi_0_delta <- compute_true_Psi0_delta("L0_exp", 2, 2, -3, 1.5, 1, alwaysTreated0, delta_n_plus)
                      
-                     true_finite_diffs_bias <- c(true_finite_diffs_bias, (Psi_0_delta_plus_Delta - Psi_0_delta) / Delta)
+                     true_finite_diffs_bias <- (Psi_0_delta_plus_Delta - Psi_0_delta) / Delta
                      
-                     print(finite_diffs)
-                     print(finite_diffs_bias)
-                     
-                     #   par(mfrow = c(1, 2))
-                     #   # plot(log(deltas), log(abs(finite_diffs)))
-                     #   plot(log(deltas), log(abs(finite_diffs_bias)))
-                     #   plot(log(deltas), log(abs(true_finite_diffs_bias)))
                      c(eta, n, finite_diffs, finite_diffs_bias, true_finite_diffs_bias)
                    }
 
 stopCluster(cl)
 
-plot(log(deltas), log(abs(finite_diffs_bias)), ylim = c(min(c(log(abs(finite_diffs_bias)), log(abs(true_finite_diffs_bias)))),
-                                                        max(c(log(abs(finite_diffs_bias)), log(abs(true_finite_diffs_bias))))),
-     main= substitute(list(eta) == list(x),
-                      list(x = eta)))
-lines(log(deltas), log(abs(true_finite_diffs_bias)))
+row.names(results) <- NULL
+colnames(results) <- c('eta', 'n', 'fin_diff', 'bias.fin_diff', 'true_bias.fin_diff')
+results <- cbind(results, delta = results[, 'n']^(- 1 / (2 * results[, 'eta'] * (gamma + 1 - beta))))
+
+results_df <- as.data.frame(rbind(cbind(results[, c('eta', 'n', 'delta')], fin_diff = results[, 'bias.fin_diff'], type = 'bias.fin_diff'),
+                                  cbind(results[, c('eta', 'n', 'delta')], fin_diff = results[, 'true_bias.fin_diff'], type = 'true_bias.fin_diff')))
+results_df <- transform(results_df, n = as.numeric(as.character(n)), 
+                        eta = as.numeric(as.character(eta)),
+                        delta = as.numeric(as.character(delta)),
+                        fin_diff = as.numeric(as.character(fin_diff)))
+
+
+plots <- list()
+for(i in 1:length(etas)){
+  data <- subset(results_df, eta == etas[i] & type == 'bias.fin_diff')
+  x1 <- min(log(data$delta)); x2 <- 0
+  y1 <- log(abs(data$fin_diff[data$delta == min(data$delta)]))
+  y2 <- y1 - 0.25 * (x2 - x1)
+  df <- data.frame(x1 = x1, x2 = x2, y1 = y1, y2 = y2)
+  plots[[i]] <- ggplot(data = data, aes(x = log(delta), y = log(abs(fin_diff)), colour = type)) + 
+    geom_line() +
+    geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2, colour = "segment"), data = df) + 
+    ggtitle(substitute(list(eta) == list(x),
+                       list(x = etas[i])))
+}
+
+all_etas.plot <- ggplot(subset(results_df, type == 'bias.fin_diff' & eta > 2.5), 
+                        aes(x = log(delta), y = log(abs(fin_diff)), colour =factor(eta))) + 
+  geom_line() +
+  geom_abline(intercept = -4, slope = -beta) +
+  geom_abline(intercept = -4.1, slope = -beta) +
+  geom_abline(intercept = -4.2, slope = -beta)
+# plot(log(deltas), log(abs(finite_diffs_bias)), ylim = c(min(c(log(abs(finite_diffs_bias)), log(abs(true_finite_diffs_bias)))),
+#                                                         max(c(log(abs(finite_diffs_bias)), log(abs(true_finite_diffs_bias))))),
+#      main= substitute(list(eta) == list(x),
+#                       list(x = eta)))
+# lines(log(deltas), log(abs(true_finite_diffs_bias)))
