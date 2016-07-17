26346aeb5f798841519f6e6ef3a50e89a7176718
Aurelien Bibaut <aurelien.bibaut@gmail.com>
2016-07-17 18:08:18 +0200

    Progress on broken line data generation

--- data-adaptive_delta_selection/broken_line_find_gamma-generate_datasets.R
+++ data-adaptive_delta_selection/broken_line_find_gamma-generate_datasets.R
@@ -10,7 +10,7 @@ data_generating_distributions.parameters[[2]] <- list(lambda = 2, alpha0 = 4, be
 # Set of parameters 3
 data_generating_distributions.parameters[[3]] <- list(lambda <= 2, alpha0 = 4, beta2 =-3, beta0 = -1, beta1 = 1,
                                                       beta = 7/8, gamma = 5/16)
-ns <- c(10^3.5, 10^4.5, 10^5.5)
+ns <- floor(c(10^3.5, 10^4.5, 10^5.5))
 
 # Define jobs
 nb_repeats <- 20
@@ -20,13 +20,16 @@ jobs <- sample(kronecker(1:nrow(parameters_grid), rep(1, nb_repeats)))
 broken_line.results <- vector()
 for(job in jobs){
   current_broken_line.result <- NULL
-  try(current_broken_line.result <- generate_data_and_gamma_broken_line('L0_exp', parameters_grid[job, ]$lambda, 
-                                                                        parameters_grid[job, ]$alpha0, 
-                                                                        parameters_grid[job, ]$beta0, 
-                                                                        parameters_grid[job, ]$beta1, 
-                                                                        parameters_grid[job, ]$beta2, 
+  current_data_generating_distributions.parameters <- data_generating_distributions.parameters[[parameters_grid[job, ]$distribution_id]]
+  
+  try(current_broken_line.result <- generate_data_and_gamma_broken_line('L0_exp', 
+                                                                        current_data_generating_distributions.parameters$lambda, 
+                                                                        current_data_generating_distributions.parameters$alpha0,
+                                                                        current_data_generating_distributions.parameters$beta0, 
+                                                                        current_data_generating_distributions.parameters$beta1, 
+                                                                        current_data_generating_distributions.parameters$beta2, 
                                                                         parameters_grid[job, ]$n, 
-                                                                        parameters_grid[job, ]$gamma)$broken_line_points_df)
+                                                                        current_data_generating_distributions.parameters$gamma)$broken_line_points_df)
   if(!is.null(current_broken_line.result)){
     cat('For job')
     print(parameters_grid[job, ])
