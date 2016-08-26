cb33e789b736ce5a81f8d4bcfbd4ad0a0194d892
Aurelien Bibaut <aurelien.bibaut@gmail.com>
2016-08-26 13:38:34 +0200

    Implement h2o regression for rate

--- data-adaptive_delta_selection/merge_results.R
+++ data-adaptive_delta_selection/merge_results.R
@@ -1,4 +1,4 @@
-results_files <- list.files(pattern = "^broken_lines.results")
+results_files <- list.files(pattern = "^rate_inference.features.results")
 broken_lines.results <- vector()
 for(results_file in results_files){
   current_results <- read.csv(results_file)
@@ -6,4 +6,4 @@ for(results_file in results_files){
     current_results[, "dataset_id"] <- current_results[, "dataset_id"] + max(broken_lines.results[, "dataset_id"])
   broken_lines.results <- rbind(broken_lines.results, current_results)
 }
-write.csv(broken_lines.results, "merged_broken_lines.results.csv")
+write.csv(broken_lines.results, "merged_rate_inference.features.results.csv")
